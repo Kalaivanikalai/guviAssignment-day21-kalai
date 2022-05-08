2e8009fd591ee1197099345e979ea4a64ec4a6c2
Kalaivani <97941263+Kalaivanikalai@users.noreply.github.com>
2022-05-08 21:50:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+function num10 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 10;
+        call();    
+    },1000)
+};
+
+function num9 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 9;
+        call();      
+    },1000)
+};
+function num8 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 8;
+        call();
+    },1000)
+};
+
+function num7 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 7;
+        call();
+    },1000)
+};
+
+function num6 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 6;
+        call();
+    },1000)
+};
+
+function num5 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 5;
+        call();
+    },1000)
+};
+
+function num4 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 4;
+        call();
+    },1000)
+};
+
+function num3 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 3;
+        call();
+    },1000)
+};
+
+function num2 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 2;
+        call();
+    },1000)
+};
+
+function num1 (call){
+    setTimeout(()=>{
+        document.getElementById("demo").textContent = 1;
+        call();
+    },1000)
+};
+
+num10(()=>{
+    num9(()=>{
+        num8(()=>{
+            num7(()=>{
+                num6(()=>{
+                    num5(()=>{
+                        num4(()=>{
+                            num3(()=>{
+                                num2(()=>{
+                                    num1(()=>{
+
+setTimeout(()=>{
+    document.getElementById("demo").innerHTML = "Happy Independance Day..!! &#128522";
+},2000);
+                                     })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
